354776e83f7a836aaf38e44d147d36ae21fc672c
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-23 08:51:27 +0530

    Refactor removeDuplicates function for clarity

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k=1;
+        int k=1; 
         for(int i=0;i<nums.size()-1;i++) {
             if(nums[i] != nums[i+1]) {
                 nums[k] = nums[i+1];
                 k++;
             }
         }
-        return k;
+        return k;   
     }
 };
