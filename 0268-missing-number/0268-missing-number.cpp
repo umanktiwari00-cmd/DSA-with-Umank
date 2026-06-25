f9d371b5522ef2bba7430244f3e0c091f71331fe
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-25 12:41:54 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.75%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int s1 = (n*(n+1))/2;
+        int s2=0;
+        for(int i=0;i<nums.size();i++) {
+            s2 += nums[i];
+
+        }
+        return s1-s2;
+    }
+};
