1891918c44625549f0b0fede92c111c66b1710ac
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-07-18 14:59:23 +0530

    Time: 0 ms (100%), Space: 15.6 MB (73.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         next_permutation(nums.begin(),nums.end()) ;
-    }
+         int idx = -1;
+         int n = nums.size();
+         // break point
+         for(int i = n-2; i>=0;i--) {
+            if(nums[i] < nums[i+1]) {
+                idx = i;
+                break;
+            }
+         }
+         if(idx == -1) {
+            reverse(nums.begin(),nums.end());
+            return;
+         }
+         for(int i=n-1;i>=1;i--) {
+            if(nums[i] > nums[idx]) {
+                swap(nums[i],nums[idx]);
+                break;
+            }
+         }
+         reverse(nums.begin()+idx+1,nums.end());
+     }
 };
