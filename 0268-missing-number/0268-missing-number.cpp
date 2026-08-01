c7c7022c5f7e7dbbdc6f884a7bb49b04fda750b5
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-08-01 19:23:54 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int s1 = (n*(n+1))/2;
-        int s2=0;
-        for(int i=0;i<nums.size();i++) {
-            s2 += nums[i];
-
-        }
-        return s1-s2;
+        int xor1 = 0,xor2 = 0;
+       for (int i = 0; i < nums.size(); i++) {
+    xor2 ^= nums[i];
+    xor1 ^= (i + 1);
+}
+return xor1 ^ xor2;
     }
 };
