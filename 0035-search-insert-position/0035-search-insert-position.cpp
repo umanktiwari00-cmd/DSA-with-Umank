52c24f525b5c7381806b0fd909978b78987cf425
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-08-19 15:35:55 +0530

    Time: 0 ms (100%), Space: 13.6 MB (78.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                return i;   
-            } else if (target < nums[i]) {
-                return i;   
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
-        return nums.size();  
+
+        return low;
     }
 };
